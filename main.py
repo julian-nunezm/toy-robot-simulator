11270f33dc8b788089c7d5139db216dc68d08ae7
Julian Nunez <julian.nunezm@gmail.com>
2020-04-13 18:42:40 +1000

    Added exceptions handles and a feature to read many files

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ from controller.command_interpreter import Controller
 
 
 def main():
-    file_name = 'test_data.txt'
-    commands = []
-    print('Toy Robot Simulator')
+    folder = 'data/'
+    data_files = [
+        'example_a.txt',
+        'example_b.txt',
+        'example_c.txt',
+        'example_k.txt',
+        'example_s.txt',
+        'test_data.txt',
+    ]
+    print('\nToy Robot Simulator\n')
     
     # Reading file
-    file = open(file_name, 'r')
-    commands = [line.replace('\n', '') for line in file.readlines()]
-    file.close()
-    
-    # Play the simulator
-    simulator = Controller()
-    simulator.play(commands)
+    for file_name in data_files:
+        commands = []
+        try:
+            print(f'File name: {file_name}')
+            file = open(folder + file_name, 'r')
+            commands = [line.replace('\n', '') for line in file.readlines()]
+
+            # Play the simulator
+            simulator = Controller()
+            simulator.play(commands)
+        except:
+            print(f'ERROR: An error over {file_name} occured. The file was ignored.')
+        finally:
+            file.close()
+            print(f'{file_name}.... done!\n')
     
     
 if __name__ == '__main__':
